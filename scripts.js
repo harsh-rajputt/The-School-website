e1b2b1add9232045bb2ffc701726b0800db54003
Harsh raj <162718961+harsh-rajputt@users.noreply.github.com>
2024-08-15 08:46:51 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdown = document.querySelector(".dropdown");
+    
+//     dropdown.addEventListener("mouseover", function() {
+//         const dropdownContent = this.querySelector(".dropdown-content");
+//         dropdownContent.style.display = "flex";
+//     });
+    
+//     dropdown.addEventListener("mouseout", function() {
+//         const dropdownContent = this.querySelector(".dropdown-content");
+//         dropdownContent.style.display = "none";
+//     });
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdown = document.querySelector(".dropdown");
+    
+//     dropdown.addEventListener("mouseover", function() {
+//         const dropdownContent = this.querySelector(".dropdown-content");
+//         dropdownContent.style.display = "flex";
+//     });
+    
+//     dropdown.addEventListener("mouseout", function() {
+//         const dropdownContent = this.querySelector(".dropdown-content");
+//         dropdownContent.style.display = "none";
+//     });
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdownContent = document.querySelectorAll(".dropdown-content");
+    
+//     // Ensure all dropdown contents are hidden on page load
+//     dropdownContent.forEach(content => {
+//         content.style.display = "none";
+//     });
+// });
+
